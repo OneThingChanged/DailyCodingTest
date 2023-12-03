ad6cc08a801fd6bea2e32b4af35700784ed1cf85
jintaenate <jintaenate@gmail.com>
2023-12-03 19:22:18 +0900

    프로그래머스
    코테
    
    요격시스템
    
    Level2
    
    https://school.programmers.co.kr/learn/courses/30/lessons/181188

--- Program/CodingTestCpp/CodingTestCpp.cpp
+++ Program/CodingTestCpp/CodingTestCpp.cpp
@@ -10,22 +10,49 @@
 #include "Level2/AIOven.h"
 #include "Level2/Clock.h"
 #include "Level2/OilDrilling.h"
+#include "Level2/AntiMissileSystem.h"
 
+class MainClass
+{
+    int Level = 2;
+
+public:
+    void Run()
+    {
+        switch (Level)
+        {
+        case 2:
+            Level2();
+            break;
+        }
+        AIOven quest;
+        quest.Question();
+    }
+
+    void Level1()
+    {
+        // SuffixArray::Answer();
+        // Templars_Weapon::Question();
+        // Running_Race::Question();
+        // Bandage::Question();
+        // Clock quest;
+        // quest.Question();
+    }
+
+    void Level2()
+    {
+        AntiMissileSystem antiMissile;
+        antiMissile.Question();
+    }
+};
 int main(int argc, char* argv[])
 {
-    // SuffixArray::Answer();
-    // Templars_Weapon::Question();
-    // Running_Race::Question();
-    // Bandage::Question();
-    // Clock quest;
-    // quest.Question();
-
-    AIOven quest;
-    quest.Question();
+    MainClass main; 
+    main.Run();
 
     // 사용자 입력을 기다립니다.
-    // std::cout << "프로그램을 종료하려면 엔터 키를 누르세요..." << std::endl;
-    // std::cin.get();
+    std::cout << "프로그램을 종료하려면 엔터 키를 누르세요..." << std::endl;
+    std::cin.get();
 
     return 0;
 }
---
+++ Program/CodingTestCpp/Level2/AntiMissileSystem.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/181188
+static class AntiMissileSystem
+{
+public:
+    
+    
+    void Question()
+    {
+        vector<vector<int>> targets = {{4, 5}, {4, 8}, {10, 14}, {11, 13}, {5, 12}, {3, 7}, {1, 4}};
+        int result2 = solution(targets);
+        cout << result2 << "," << endl;
+    }
+    
+int solution(vector<vector<int>> targets) {
+    int answer = 0;
+
+    //시작점 s를 기준으로 정렬
+    sort(targets.begin(), targets.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
+    });
+
+    int size = targets.size();
+    int max_e = -1;
+    for (int i = 0; i < size; ++i)
+    {
+        int cur_s = targets[i][0];
+        int cur_e = targets[i][1];
+
+        if (cur_s >= max_e)
+        {
+            max_e = cur_e;
+            answer++;
+        }
+    }
+    
+    return answer;
+}
+};
