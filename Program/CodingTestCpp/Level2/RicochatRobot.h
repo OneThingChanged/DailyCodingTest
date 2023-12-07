32ffc0d84f3758230a3efcc8735c6c6b8510a9b3
jintaenate <jintaenate@gmail.com>
2023-12-08 03:14:59 +0900

    봇임

--- Program/CodingTestCpp/Level2/RicochatRobot.h
+++ Program/CodingTestCpp/Level2/RicochatRobot.h
@@ -12,12 +12,7 @@ class RicochatRobot
 {
 public:
 
-    struct Pos
-    {
-        int x = 0;
-        int y = 0;
-        int dist = 0;
-    };
+    
     void Question()
     {
         vector<string> board1 = {"...D..R", ".D.G...", "....D.D", "D....D.", "..D...."};
@@ -26,7 +21,52 @@ public:
         vector<string> board2 = {".D.R", "....", ".G..", "...D"};
         cout << solution(board2) << endl; // Output: -1
     }    
-    
+
+    struct Pos
+    {
+        int x = 0;
+        int y = 0;
+        int dist = 0;
+    };
+
+    bool AbleToMove(int x, int y, vector<string>& board)
+    {
+        return x < board.size() && y < board[x].size() && board[x][y] != 'D';  
+    }
+
+    int BFS(vector<string> board, int n, int m, int dx[], int dy[], Pos startPos, Pos endPos)
+    {
+        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        queue<Pos> q;
+        q.push(startPos);
+        visited[startPos.x][startPos.y] = true;
+        while (!q.empty())
+        {
+            Pos cur = q.front();
+            q.pop();
+            if (cur.x == endPos.x && cur.y == endPos.y)
+                return cur.dist;
+            
+            for (int i = 0; i < 4; ++i) {
+                int nx = cur.x;
+                int ny = cur.y;
+
+                while (AbleToMove(nx + dx[i], ny + dy[i], board))
+                {
+                    nx = nx + dx[i];
+                    ny = ny + dy[i];
+                }
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && board[nx][ny] != 'D') {
+                    visited[nx][ny] = true;
+                    q.push({nx, ny, cur.dist + 1});
+                }
+            }
+
+            
+        }
+        return -1;
+    }
+
     // board_len은 배열 board의 길이입니다.
     // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
     int solution(vector<string> board) {
@@ -59,37 +99,6 @@ public:
                 }
             }
         }
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
-        queue<Pos> q;
-        q.push(startPos);
-        while (!q.empty())
-        {
-            Pos cur = q.front();
-            q.pop();
-            if (cur.x == endPos.x && cur.y == endPos.y)
-                return cur.dist;
-            for (int i = 0; i < 4; ++i) {
-                int nx = cur.x + dx[i];
-                int ny = cur.y + dy[i];
-
-                while (AbleToMove(nx + dx[i], ny + dy[i], board))
-                {
-                    nx = nx + dx[i];
-                    ny = ny + dy[i];
-                }
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && !visited[nx][ny] && board[nx][ny] != 'D') {
-                    visited[nx][ny] = true;
-                    q.push({nx, ny, cur.dist + 1});
-                }
-            }
-
-            
-        }
-        return -1;
-    }
-
-    bool AbleToMove(int x, int y, vector<string>& board)
-    {
-        return x < board.size() && y < board[x].size() && board[x][y] != 'D';  
+        return BFS(board, n, m, dx, dy, startPos, endPos);
     }
 };
